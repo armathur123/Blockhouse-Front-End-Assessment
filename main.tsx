f3f91cd84dc23a62cd8e342e7b753714a6521dea
unknown <mathurarjun07@gmail.com>
2024-01-18 23:35:12 -0600

    aphrodite works

--- main.tsx
+++ main.tsx
@@ -1,5 +1,22 @@
-/* eslint-disable react/no-deprecated */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import App from './app'
-ReactDOM.render(<App />, document.getElementById('root'))
+import * as ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+import './index.css'
+import Homepage from './src/Homepage/Homepage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Homepage />
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
---
+++ src/Homepage/Homepage.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { StyleSheet, css } from 'aphrodite'
+
+const Homepage: React.FC = () => {
+  return (
+    <div>
+        <span className={css(styles.red)}>
+            This is red.
+        </span>
+        <span className={css(styles.hover)}>
+            This turns red on hover.
+        </span>
+        <span className={css(styles.small)}>
+            This turns red when the browser is less than 600px width.
+        </span>
+        <span className={css(styles.red, styles.blue)}>
+            This is blue.
+        </span>
+        <span className={css(styles.blue, styles.small)}>
+            This is blue and turns red when the browser is less than
+            600px width.
+        </span>
+    </div>
+  )
+}
+
+const styles = StyleSheet.create({
+  red: {
+    backgroundColor: 'red'
+  },
+
+  blue: {
+    backgroundColor: 'blue'
+  },
+
+  hover: {
+    ':hover': {
+      backgroundColor: 'red'
+    }
+  },
+
+  small: {
+    '@media (max-width: 600px)': {
+      backgroundColor: 'red'
+    }
+  }
+})
+
+export default Homepage
